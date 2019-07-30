a9e9c4b6935ead63a05bb6e64209b1c7eed13ec1
YonekanNexSeed <kyoneda@nexseed.net>
2019-07-30 11:35:21 +0800

    Main.storyboard完成 必要なクラスファイル作成

---
+++ RealmTodo/Controllers/InputViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InputViewController.swift
+//  RealmTodo
+//
+//  Created by yonekan on 2019/07/30.
+//  Copyright © 2019 yonekan. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RealmTodo/Objects/Todo.swift
@@ -0,0 +1,9 @@
+//
+//  Todo.swift
+//  RealmTodo
+//
+//  Created by yonekan on 2019/07/30.
+//  Copyright © 2019 yonekan. All rights reserved.
+//
+
+import Foundation
